4813773ec18bbe2b47265b8a8998571b2f5801c5
Iñaki Arenaza <inaki.arenaza@biotz.io>
2024-07-22 13:33:40 +0200

    Prepare build for upcoming changes in readthedocs.org
    This commits adds the changes that will be needed after "Read the Docs
    Addons" are enabled by default. See
    https://about.readthedocs.com/blog/2024/07/addons-by-default/ for
    additional details.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,20 @@
 # Configuration file for the Sphinx documentation builder.
 
+# -- Changes needed after "Read the Docs Addons" were enabled by
+# -- default. See
+# -- https://about.readthedocs.com/blog/2024/07/addons-by-default/
+# -- for additional eetails.
+import os
+
+# Define the canonical URL if you are using a custom domain on Read the Docs
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+
+# Tell Jinja2 templates the build is running on Read the Docs
+if os.environ.get("READTHEDOCS", "") == "True":
+    if "html_context" not in globals():
+        html_context = {}
+    html_context["READTHEDOCS"] = True
+
 # -- Project information
 
 project = 'HOP'
